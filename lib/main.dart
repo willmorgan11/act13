978fe72caef418b2ca4ebaed899bb83511934849
William Morgan <wmorgan2023@gmail.com>
2026-04-08 16:28:15 -0400

    Updated main.dart to use material 3 blue theme, StreamBuilder to listen to authentication state changes, and checks if the user is signed in to determine if they start at the authentication screen or profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'screens/authentication_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
@@ -15,9 +20,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Auth App',
       debugShowCheckedModeBanner: false,
-      home: AuthenticationScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: StreamBuilder(
+        // Listen to authentication state changes
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          // Wait for connection
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          // Check if user is signed in
+          if (snapshot.hasData && snapshot.data != null) {
+            // User is signed in - show profile screen
+            return const ProfileScreen();
+          }
+
+          // User is not signed in - show authentication screen
+          return const AuthenticationScreen();
+        },
+      ),
     );
   }
 }
